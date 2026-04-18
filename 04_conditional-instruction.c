a0aec1cec62837c2fec69cc587d9b1142fb337a9
Aakash gupta <aakashsahuu0188@gmail.com>
2026-04-18 22:31:36 +0530

    Add files via upload

---
+++ 04_conditional-instruction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int age =15;
+    if (age>10){
+
+    
+         printf("we are inside if \n") ;
+         printf("your age is greater than 10\n");
+    }
+    if (age%5==0){
+
+    
+         printf("we are inside  anotherif \n") ;
+         printf("your age is divisible by  50\n");
+    }
+
+
+    
+
+    
+    return 0;
+}
---
+++ 06_ternary.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a=345;
+    int b=34567;
+    a>b?printf("a is graeter"):printf("b is greater");
+
+    return 0;
+}
