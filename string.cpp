be08a5cad1630a641954092d5ee8537a7eef1d34
Aakash gupta <aakashsahuu0188@gmail.com>
2026-04-28 21:06:51 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,43 @@
+
+#include<string>
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+
+    string name="sky";
+
+
+    cout << "The name "<<name<<endl;
+    cout << "The length of name "<<name.length()<<endl;
+    cout << "The name is "<<name.substr(0,3)<<endl;
+
+
+
+
+    return 0;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
