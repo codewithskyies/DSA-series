f7110b2b493eb7f5e88236edd6b097fba5520fc8
Aakash gupta <aakashsahuu0188@gmail.com>
2026-04-28 21:05:48 +0530

    Add files via upload

---
+++ 01.taking-user-input.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cout <<" Enter the number: "<<endl;
+    cin>>a;
+
+
+    cout <<" Enter the number: "<<endl;
+    cin>>b;
+
+    cout <<" The sum is "<<a+b <<endl;
+    cout <<" The sum is "<<a+b <<endl;
+    cout <<" The sum is "<<a+b <<endl;
+    cout <<" The sum is "<<a+b <<endl;
+
+    
+
+
+
+
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
