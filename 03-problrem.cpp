317d79bacf9b3f4baba230f854d572ad536f5d8d
Aakash gupta <aakashsahuu0188@gmail.com>
2026-05-03 23:41:14 +0530

    Add files via upload

---
+++ 03-problrem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter the first number: ";
+    cin >> a;
+
+    cout << "Enter the second number: ";
+    cin >> b;
+
+    cout << "The sum is " << a + b << endl;
+    cout << "The sum is " << a - b << endl;
+    cout << "The sum is " << a * b << endl;
+    cout << "The sum is " << a / b << endl;
+
+    return 0;
+}
