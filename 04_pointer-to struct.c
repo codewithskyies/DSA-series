4e38385e0468cbb77f7751854fb630653b4555a6
Aakash gupta <aakashsahuu0188@gmail.com>
2026-05-07 23:00:26 +0530

    Add files via upload

---
+++ 04_pointer-to struct.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+struct employee
+{
+    int code;
+    float salary;
+    char name[10];
+};
+
+int main()
+{
+    struct employee e1;
+
+    e1.code = 34;
+    struct employee *ptr;
+    ptr = &e1;
+    // printf("%d\n", (*ptr).code);
+    printf("%d\n", ptr->code);
+
+    return 0;
+}
