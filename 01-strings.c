fbf979851f1203f71137deea9d153a9ef5226258
Aakash gupta <aakashsahuu0188@gmail.com>
2026-06-19 09:19:14 +0530

    Add files via upload

---
+++ 01-strings.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // char str []={'a','b','c','\0'};
+    char st[]="abc";
+    for (int i=0;i<3;i++){
+    printf("First charater is %c \n",st [i]);
+    }
+    return 0;
+}
